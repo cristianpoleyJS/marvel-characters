49446165eb0aa6e4930784f88bd6cfa84d39c567
Cristian Poley <cripolgon@gmail.com>
2019-03-12 21:11:42 +0100

    [feat] Added get service and print items

---
+++ src/components/Character.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="character">
+    {{ character.name }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Character',
+  props: {
+    character: Object,
+    default: () => {
+      return { }
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+</style>
