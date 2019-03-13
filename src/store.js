9fc24c2de93f01dc8e5d0f8cf4d4d279e9aa48ad
Cristian Poley <cripolgon@gmail.com>
2019-03-13 20:51:12 +0100

    [feat] Added mutations and getters in store.js

--- src/store.js
+++ src/store.js
@@ -5,12 +5,41 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    characters: [ ],
+    busy: false
+  },
+  getters: {
 
+    /**
+     * Return array marvel characters with current input search.
+     * @returns {Boolean}
+     */
+    getCharacters: state => state.characters,
+
+    /**
+     * Return value busy var, if busy is true, show Spinner.
+     * @returns {Boolean}
+     */
+    getBusy: state => state.busy
   },
   mutations: {
 
-  },
-  actions: {
+    /**
+     * Modify value 'characters' with new list of return service API.
+     * @param {Object} state State app.
+     * @param {Array} value New characters (max 12 elements).
+     */
+    setCharacters (state, value) {
+      state.characters = value
+    },
 
+    /**
+     * Set busy when getting data service API.
+     * @param {Object} state State app.
+     * @param {Boolean} value New value.
+     */
+    setBusy (state, value) {
+      state.busy = value
+    }
   }
 })
